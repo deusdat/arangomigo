6d16bcd12f2b3daa9938c06181bd3a9827f4d3ba
grahamlyons <graham.lyons_ext@50hertz.com>
2024-04-18 14:07:47 +0100

    Add ARM build to the build script

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ GOOS=windows  GOARCH=amd64  go build -o builds/arangomigo-amd64.exe     cmd/aran
 GOOS=windows  GOARCH=386    go build -o builds/arangomigo-386.exe       cmd/arangomigo/main.go
 GOOS=darwin   GOARCH=amd64  go build -o builds/arangomigo-amd64-darwin  cmd/arangomigo/main.go
 GOOS=linux    GOARCH=amd64  go build -o builds/arangomigo-amd64-linux   cmd/arangomigo/main.go
+GOOS=linux    GOARCH=arm64  go build -o builds/arangomigo-arm64-linux   cmd/arangomigo/main.go
 GOOS=linux    GOARCH=386    go build -o builds/arangomigo-386-linux     cmd/arangomigo/main.go
